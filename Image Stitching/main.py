595c9d1e331784fab4283c47520eefa83ffcc1e3
Daniel Brasil <mrbrasildaniel@gmail.com>
2025-08-08 23:46:56 -0300

    add greyscale images load

--- Image Stitching/main.py
+++ Image Stitching/main.py
@@ -1,12 +1,18 @@
 import cv2
 import numpy as np
 import os
+import glob
 
-def load_data(path:str = 'data/raw'):
+def load_data(path:str):
     imgs = []
     for img_path in os.listdir(path):
         img = cv2.imread(os.path.join(path,img_path))
+        img = cv2.resize(img, (0,0),fx=.2, fy=.2)
         imgs.append(img)
+        cv2.imshow("Image",img)
+        cv2.waitKey(0)
+    cv2.destroyAllWindows()
     return imgs
 
 # feature detection
+load_data('Image Stitching/data/raw')
