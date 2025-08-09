9d4cb359489cd14b75996d5d53e1b79b31edf0e2
Daniel Brasil <mrbrasildaniel@gmail.com>
2025-08-08 23:51:51 -0300

    changes in the load function

--- Image Stitching/main.py
+++ Image Stitching/main.py
@@ -1,18 +1,20 @@
 import cv2
 import numpy as np
 import os
-import glob
+import matplotlib.pyplot as plt
 
 def load_data(path:str):
     imgs = []
-    for img_path in os.listdir(path):
+    greys = []
+    for img_path in sorted(os.listdir(path)):
         img = cv2.imread(os.path.join(path,img_path))
         img = cv2.resize(img, (0,0),fx=.2, fy=.2)
-        imgs.append(img)
-        cv2.imshow("Image",img)
-        cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    return imgs
+        imgs.append(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        greys.append(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+        plt.imshow(img)
+        plt.axis('off')
+        plt.show()
+    return imgs, greys
 
 # feature detection
 load_data('Image Stitching/data/raw')
